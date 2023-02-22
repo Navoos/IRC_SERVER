09cb2b2b90743144fbaae77b97fedd0132aa89b4
Houssam Lalouli <lalouli.houssam2017@gmail.com>
2023-02-22 20:23:02 +0100

    class Mode

--- src/part_cmd.class.cpp
+++ src/part_cmd.class.cpp
@@ -5,5 +5,5 @@ Part::Part(Client* client, Channel* channel) : client(client), channel(channel) 
 Part::~Part(){}
 
 void Part::execute(Mediator* mediator) {
-        mediator->part(client, channel);
+        mediator->part_cmd(client, channel);
     }
